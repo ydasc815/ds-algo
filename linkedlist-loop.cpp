a2d1fdb5712fa4b7fd0486bc57f3197b78505e58
Aditya <icgadi99@gmail.com>
2021-04-29 03:24:19 +0530

    loop detection (n/w)

---
+++ linkedlist-loop.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define null NULL
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node* next = null;
+    Node* curr_ptr = this;
+	Node(int d) {
+		this->data = d;
+	}
+	Node() {}
+};
+
+void build_list(Node** head, int d) {
+	if((*head) == null) {
+		// it is head node
+		*head = new Node();
+		(*head)->data = d;
+	} else {
+		Node* newNode = new Node();
+		newNode->data = d;
+		newNode->next = null;
+		Node* temp = *head;
+		while(temp->next != null) {
+			temp = temp->next;
+		}
+		temp->next = newNode;
+	}
+}
+
+bool detect_loop(Node* head){
+	Node* hptr = head;
+    map<Node*, int> m;
+	while(hptr != null) {
+        m[hptr->curr_ptr]++;
+		hptr = hptr->next;
+	}
+    for(auto it=m.begin(); it!=m.end(); it++) {
+        if(it->second > 1) return true;
+    }
+    return false;
+}
+
+int main() {
+    Node* head = null;
+    build_list(&head, 1);
+	build_list(&head, 3);
+	build_list(&head, 2);
+	build_list(&head, 5);
+
+    head->next->next->next->next = head; // test loop
+
+    if(detect_loop(head)) cout<<"loop found !"<<"\n";
+    else cout<<"loop not found !"<<"\n";
+
+}
