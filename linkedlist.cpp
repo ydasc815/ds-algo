2645ed0006405a744984c137a22b9933ec10267d
Aditya <icgadi99@gmail.com>
2021-04-28 10:05:57 +0530

    list size

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -20,6 +20,7 @@ void build_list(Node** head, int d) {
 	} else {
 		Node* newNode = new Node();
 		newNode->data = d;
+		newNode->next = null;
 		Node* temp = *head;
 		while(temp->next != null) {
 			temp = temp->next;
@@ -28,21 +29,76 @@ void build_list(Node** head, int d) {
 	}
 }
 
+void pop_front(Node** head) {
+    if(*head == null) {
+        // means list is empty
+        cout<<"Empty List !"<<"\n";
+        return;
+    } else {
+        Node* temp1 = *head, *temp;
+        while(temp1->next != null) {
+            temp = temp1;
+            temp1 = temp1->next;
+        }
+        temp->next = null;
+        free(temp1);
+    }
+}
+
+void pop_back(Node** head) {
+    if(*head == null) {
+        // means list is empty
+        cout<<"Empty List !"<<"\n";
+        return;
+    } else {
+        Node* temp = *head;
+        *head = (*head)->next;
+        temp->next = null;
+        free(temp);
+    }
+}
+
+void delete_list(Node** head) {
+    Node* temp = *head, *temp1 = temp;
+    while(temp != null) {
+        temp = temp->next;
+        free(temp1);
+        temp1 = temp;
+    }
+    *head = null;
+}
+
+int get_size(Node** head) {
+	int size = 0;
+	Node* temp = *head;
+	while(temp != null) {
+		size++;
+		temp = temp->next;
+	}
+	return size;
+}
+
 void print_list(Node** head){
 	Node* hptr = *head;
 	while(hptr != null) {
 		cout<<hptr->data<<" ";
 		hptr = hptr->next;
 	}
+	cout<<"\n";
 }
 
 int main() {
-	Node* head = null; int data;
+	Node* head = null;
 	// building a singly linkedlist with user input data [size = 10]
-	for(int i=0; i<10; i++){
-		cin>>data;
-		build_list(&head, data);
-	}
+	build_list(&head, 1);
+	build_list(&head, 3);
+	build_list(&head, 2);
+	build_list(&head, 5);
+	build_list(&head, 9);
+	build_list(&head, 7);
+	build_list(&head, 6);
+	build_list(&head, 4);
 
 	print_list(&head);
+	cout<<"size of given list = "<<get_size(&head)<<"\n";
 }
