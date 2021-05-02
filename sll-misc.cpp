d1bab647ce8fe37a1179eb5b07a0652348c70f05
Aditya <icgadi99@gmail.com>
2021-05-02 16:40:12 +0530

    remove duplicates from sorted sll

--- sll-misc.cpp
+++ sll-misc.cpp
@@ -148,3 +148,73 @@
 //     print_list(head);
 
 // }
+
+#include<bits/stdc++.h>
+#define null NULL
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int d) {
+        this->data = d;
+    }
+    Node() {}
+};
+
+void build_list(Node** head, int d) {
+    if(*head == null) {
+        *head = new Node();
+        (*head)->data = d;
+        (*head)->next = null;
+    } else {
+        Node* newNode = new Node();
+        Node* temp = *head;
+        newNode->next = null;
+        newNode->data = d;
+        while(temp->next != null) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void print_list(Node* head) {
+    Node* temp = head;
+    while(temp != null) {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<"\n";
+}
+
+void remove_duplicates(Node* head) {
+    Node* temp = head;
+    while(temp->next != null) {
+        if(temp->data == temp->next->data) {
+            Node* delTemp = temp;
+            delTemp = delTemp->next;
+            temp->next = delTemp->next;
+            delTemp->next = null;
+            delete(delTemp);
+        } else temp = temp->next;
+    }
+}
+
+int main() {
+    // always initialize empty list (= null)
+    Node* head = null;
+    build_list(&head, 11);
+    build_list(&head, 11);
+    build_list(&head, 11);
+    build_list(&head, 21);
+    build_list(&head, 43);
+    build_list(&head, 43);
+    build_list(&head, 60);
+    build_list(&head, 60);
+    remove_duplicates(head);
+
+    print_list(head);
+
+}
