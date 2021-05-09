b8f3f6b58ae886330fcfac2d51500604a1cdce4e
Aditya <icgadi99@gmail.com>
2021-05-09 20:06:58 +0530

    binary tree traversals

---
+++ binary tree/traversals.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#define null NULL
+using namespace std;
+
+class Node {
+    public: 
+    int data;
+    Node *left, *right;
+    // create a constructor so as to avoid defining
+    // left and right nodes as null, everytime a node is created.
+    Node(int data) {
+        this->data = data;
+        this->left = this->right = null;
+    }
+};
+
+void inorder(Node* node) {
+    if(node == null) return;
+
+    inorder(node->left);
+    cout<<node->data<<" ";
+    inorder(node->right);
+}
+
+void preorder(Node* node) {
+    if(node == null) return;
+
+    cout<<node->data<<" ";
+    preorder(node->left);
+    preorder(node->right);
+}
+
+void postorder(Node* node) {
+    if(node == null) return;
+
+    postorder(node->left);
+    postorder(node->right);
+    cout<<node->data<<" ";
+}
+
+int main() {
+    Node* root = new Node(2);
+    root->left = new Node(1);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(9);
+
+    // printing inorder traversal of binary tree
+    inorder(root);
+    cout<<"\n";
+
+    // printing preorder traversal of binary tree
+    preorder(root);
+    cout<<"\n";
+
+    // printing postorder traversal of binary tree
+    postorder(root);
+    cout<<"\n";
+}
--- stack/stack-basics.cpp
+++ stack/stack-basics.cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<int> v;
+int fib(int n) {
+        if(n == 0 || n == 1) v.push_back(1);
+        else v.push_back(n + fib(n-1));
+    }
 int main() {
-    
+    cout<<fib(2);
 }
