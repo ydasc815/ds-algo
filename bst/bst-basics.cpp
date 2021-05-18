0bab2be6092b38ad9fe1ee829212f26b18d1110c
Aditya <icgadi99@gmail.com>
2021-05-18 15:06:10 +0530

    bst insertion

--- bst/bst-basics.cpp
+++ bst/bst-basics.cpp
@@ -19,6 +19,29 @@ void inorder(BST* root) {
     inorder(root->right);
 }
 
-void bst_insert(BST root, int data) {
-    
+BST* bst_insert(BST *root, int data) {
+    if(root == null) {
+        root = new BST(data);
+        return root;
+    }
+    if(data < root->data) {
+        root->left = bst_insert(root->left, data);
+    } else if(data > root->data) {
+        root->right = bst_insert(root->right, data);
+    }
+    return root;
+}
+
+int main() {
+    BST* root = new BST(4);
+    root->left = new BST(2);
+    root->right = new BST(5);
+    root->left->left = new BST(0);
+    root->left->right = new BST(3);
+
+    inorder(root);
+    cout<<"\n";
+
+    inorder(bst_insert(root, 1));
+
 }
