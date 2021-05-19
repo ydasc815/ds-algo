e285a19e2f04f585136753fbdc17f77eefbe912a
Aditya <icgadi99@gmail.com>
2021-05-19 14:48:07 +0530

    test level wise traversal

--- binary tree/level-wise-traversal.cpp
+++ binary tree/level-wise-traversal.cpp
@@ -27,13 +27,16 @@ void levelorder(Node* node, int level) {
 }
 
 int main() {
-    Node* root = new Node(2);
-    root->left = new Node(3);
-    root->right = new Node(1);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(8);
-    root->right->right = new Node(7);
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(10);
+    root->left->right = new Node(4);
+    root->right->right = new Node(9);
+    root->left->right->left = new Node(5);
+    root->left->right->left->left = new Node(6);
+    root->left->right->left->right = new Node(8);
+    root->left->right->left->left->right = new Node(7);
 
     int h = height(root);
     for(int i=1; i<=h; i++) {
