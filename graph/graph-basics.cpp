66d46698018f0c191d505500ab81a7f810ab07e4
Aditya <icgadi99@gmail.com>
2021-06-04 09:14:21 +0530

    bfs of graph

--- graph/graph-basics.cpp
+++ graph/graph-basics.cpp
@@ -1,15 +1,66 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n, m;
+//     cin>>n>>m;
+    
+//     vector<int> adj[n+1];
+//     for(int i=0; i<m; i++){
+//         int u, v;
+//         cin>>u>>v;
+
+//         adj[u].push_back(v);
+//         adj[v].push_back(u);
+//     }
+// }
+
+// bfs of graph
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int n, m;
-    cin>>n>>m;
-    
-    vector<int> adj[n+1];
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin>>u>>v;
 
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+vector<int> bfs(int V, vector<int> adj[]) {
+    int vis[V+1] = {0};
+    vector<int> res;
+
+    for(int i=1; i<=V; i++){
+        if(!vis[i]){
+            queue<int> q;
+            q.push(i);
+            vis[i] = 1;
+            while(!q.empty()){
+                int f = q.front();
+                q.pop();
+                res.push_back(f);
+
+                for(auto x : adj[f]){
+                    if(!vis[x]){
+                        q.push(x);
+                        vis[x] = 1;
+                    }
+                }
+            }
+        }       
+    }
+    return res;
+}
+
+int main(){
+    int V = 7;
+    vector<int> adj[V+1];
+    adj[1].push_back(2);
+    adj[2].push_back(1);
+    adj[2].push_back(3);
+    adj[2].push_back(7);
+    adj[3].push_back(2);
+    adj[3].push_back(5);
+    adj[4].push_back(6);
+    adj[5].push_back(3);
+    adj[5].push_back(7);
+    adj[6].push_back(4);
+    adj[7].push_back(2);
+    adj[7].push_back(5);
+    vector<int> trv = bfs(V, adj);
+    for(int i : trv){
+        cout<<i<<" ";
     }
 }
